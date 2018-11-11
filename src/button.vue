6cad36dcf1ff7913cadcab356c2c966ca04183a1
yuyu <a837477816@qq.com>
2018-11-11 13:15:59 +0800

    添加icon & iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,21 +1,35 @@
 <template>
-    <button class="yu-button">按钮</button>
+    <button 
+        class="yu-button"
+        :class="{[`icon-${iconPosition ? iconPosition : 'left'}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
 export default {
-    name: 'yu-button'
+    name: 'yu-button',
+    props: ['icon', 'iconPosition']
 }
 </script>
 
 <style lang="scss">
     .yu-button{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        vertical-align: middle;
         &:hover{
             border-color: var(--border-color-hover)
         }
@@ -24,6 +38,23 @@ export default {
         }
         &:focus{
             outline: none;
+        }
+        > .icon{
+            order: 1;
+            margin-right: .3em;
+        }
+        > .content{
+            order: 2;
+        }
+        &.icon-right{
+            > .content{
+                order: 1
+            }
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
         }
     }
 </style>
