5e1a89fde60d41544b6ab2ae95627ba875943c27
yuyu <a837477816@qq.com>
2018-11-12 00:23:11 +0800

    检测子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,7 +6,14 @@
 
 <script>
 export default {
-    
+    mounted() {
+        for(let node of this.$el.children) {
+            let name = node.nodeName.toLowerCase()
+            if(name !== 'button') {
+                console.warn(`yu-button-group 的子元素应该全是 yu-button，但是你写的是 ${name}`)
+            }
+        }
+    }
 }
 </script>
 
