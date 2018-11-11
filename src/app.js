58b3e3a1e19a2ccef4452375bf8dcc9bfbbaceb5
yuyu <a837477816@qq.com>
2018-11-12 00:16:14 +0800

    完成button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('yu-button', Button)
 Vue.component('yu-icon', Icon)
+Vue.component('yu-button-group', ButtonGroup)
 new Vue({
     el: '#app',
     data: {
---
+++ src/button-group.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="yu-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    .yu-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .yu-button{
+            border-radius: 0;
+            // &:not(:first-child){
+            //     border-left: none;
+            // }
+            margin-left: -1px;
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
+
+
