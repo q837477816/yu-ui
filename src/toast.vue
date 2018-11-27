cc4ff92feaa1389679188bff676166216f670f11
yuyu <a837477816@qq.com>
2018-11-27 23:06:04 +0800

    实现 position：top/bottom/middle

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast" ref="wrapper">
+    <div class="toast" ref="wrapper" :class="toastClasses">
         <div class="message">
             <slot v-if="!enableHtml"></slot>
             <div v-else v-html="$slots.default[0]"></div>
@@ -35,6 +35,18 @@ export default {
         enableHtml: {
             type: Boolean,
             default: false
+        },
+        position: {
+            type: String,
+            default: 'top',
+            validator(value) {
+                return ['top', 'bottom', 'middle'].includes(value)
+            }
+        }
+    },
+    computed: {
+        toastClasses() {
+            return [`position-${this.position}`]
         }
     },
     mounted() {
@@ -77,9 +89,7 @@ export default {
         line-height: 1.8;
         min-height: $toast-min-height;
         position: fixed;
-        top: 0;
         left: 50%;
-        transform: translateX(-50%);
         display: flex;
         align-items: center;
         color: white;
@@ -98,6 +108,18 @@ export default {
         .line {
             border-left: 1px solid #666;
             margin-left: 16px;
+        }
+        &.position-top {
+            top: 0;
+            transform: translateX(-50%);
+        }
+        &.position-bottom {
+            bottom: 0;
+            transform: translateX(-50%);
+        }
+        &.position-middle {
+            top: 50%;
+            transform: translate(-50%, -50%);
         }
     }
 </style>
