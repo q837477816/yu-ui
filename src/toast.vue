44a48aa6500008a776b3bcdd8d367b1de48a5457
yuyu <a837477816@qq.com>
2018-11-27 23:29:52 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -68,6 +68,7 @@ export default {
         },
         close() {
             this.$el.remove()
+            this.$emit('close')
             this.$destroy()
         },
         onClickClose() {
@@ -84,7 +85,18 @@ export default {
     $font-size: 14px;
     $toast-min-height: 40px;
     $toast-bg: rgba(0, 0, 0, 0.75);
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+            transform: translateY(100%)
+        }
+        100% {
+            opacity: 100%;
+            transform: translateY(0%)
+        }
+    }
     .toast {
+        animation: fade-in 1s;
         font-size: $font-size;
         line-height: 1.8;
         min-height: $toast-min-height;
