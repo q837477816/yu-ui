5c78b52bd6bd70dac08aa030ea57f0e90309cdef
yuyu <a837477816@qq.com>
2019-01-15 22:35:06 +0800

    实现基本的popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content" ></slot>
         </div>
         <slot></slot>
@@ -18,6 +18,15 @@ export default {
     methods: {
         xxx() {
             this.visible = !this.visible;
+            if (this.visible) {
+                this.$nextTick(() => {
+                    let eventHandle = () => {
+                        this.visible = false;
+                        document.removeEventListener('click', eventHandle)
+                    }
+                    document.addEventListener('click', eventHandle)
+                })
+            }
         }
     }
 }
