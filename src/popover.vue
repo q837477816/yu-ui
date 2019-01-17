b0f930c908a7ada8e2cfd7c9c1cc00de6d8bd26b
yuyu <a837477816@qq.com>
2019-01-17 21:20:00 +0800

    解决当容器有 overflow: hidden造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content" ></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -15,11 +17,19 @@ export default {
             visible: false
         }
     },
+    mounted() {
+        console.log(this.$refs.triggerWrapper)
+    },
     methods: {
         xxx() {
             this.visible = !this.visible;
             if (this.visible) {
                 this.$nextTick(() => {
+                    document.body.appendChild(this.$refs.contentWrapper)
+                    let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                    console.log(width, height, top, left)
+                    this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                    this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                     let eventHandle = () => {
                         this.visible = false;
                         document.removeEventListener('click', eventHandle)
@@ -27,6 +37,9 @@ export default {
                     document.addEventListener('click', eventHandle)
                 })
             }
+        },
+        zzz() {
+            console.log('zzz')
         }
     }
 }
@@ -37,12 +50,11 @@ export default {
         display: inline-block;
         vertical-align: top;
         position: relative;
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
+    }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%)
     }
 </style>
