74a31e2567f5cc28479a3c244495accb7378423b
yuyu <a837477816@qq.com>
2019-02-21 21:54:11 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content" ></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block;">
             <slot></slot>
         </span>
     </div>
@@ -57,6 +57,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
@@ -64,8 +66,31 @@ export default {
     }
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        transform: translateY(-100%)
+        border: 1px solid $border-color;
+        border-radius:  $border-radius;
+        filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+        background-color: #fff;
+        transform: translateY(-100%);
+        padding: 0.5em 1em;
+        margin-top: -10px;
+        max-width: 20em;
+        word-break: break-all;
+        &::before, &::after {
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+        &::before {
+            border-top-color: black;
+            top: 100%;
+        }
+        &::after {
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
