1b72eb2796eaf3705e707e92fff99b6d4c029d0c
yuyu <a837477816@qq.com>
2019-02-24 23:31:11 +0800

    初步完成collapse组件基本样式和功能

---
+++ src/collapse.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'YuCollapse'
+}
+</script>
+
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+.collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+}
+</style>
+
+
