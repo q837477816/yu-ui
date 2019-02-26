b8fd90becbaca933b1841a9b5096658332edde31
yuyu <a837477816@qq.com>
2019-02-26 21:02:32 +0800

    创建文档官网

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <yu-button>默认按钮</yu-button>
+        <yu-button icon="settings">默认按钮</yu-button>
+        <yu-button :loading="true">默认按钮</yu-button>
+        <yu-button disabled>默认按钮</yu-button>
+        
+        <pre>
+            <code>{{usage}}</code>
+        </pre>
+    </div>
+
+</template>
+
+<script>
+import Button from '../../../src/button'
+export default {
+    name: 'ButtonDemos',
+    components: {
+        'yu-button': Button
+    },
+    data() {
+        return {
+            usage: `
+                <yu-button>默认按钮</yu-button>
+                <yu-button icon="settings">默认按钮</yu-button>
+                <yu-button :loading="true">默认按钮</yu-button>
+                <yu-button disabled>默认按钮</yu-button>
+            `
+        }
+    }
+    
+}
+</script>
+
--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -20,7 +20,7 @@ export default {
         name: {
             type: String,
             required: true
-        }
+        } 
     },
     inject: ['eventBus'],
     data() {
