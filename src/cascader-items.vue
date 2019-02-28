15df43d4999d97ebf893708fa0b2b101abfa29de
yuyu <a837477816@qq.com>
2019-02-28 22:02:08 +0800

    使用递归渲染组件的方式来展示内容

---
+++ src/cascader-items.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="cascader-items">
+        <div class="left">
+            <div class="label" v-for="item in items" @click="leftSelected = item">
+                {{item.name}}
+            </div>
+        </div>
+        <div class="right" v-if="rightItems">
+            <yu-cascader-items :items="rightItems"></yu-cascader-items>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'YuCascaderItems',
+    
+    props: {
+        items: {
+            type: Array
+        }
+    },
+
+    data() {
+        return {
+            leftSelected: null
+        }
+    },
+
+    computed: {
+        rightItems() {
+            if (this.leftSelected && this.leftSelected.children) {
+                return this.leftSelected.children
+            } else {
+                return null
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.cascader-items {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    .left {
+        border: 1px solid red;
+    }
+    .right {
+        margin-top: -1px;
+    }
+}
+</style>
+
+
--- src/cascader.vue
+++ src/cascader.vue
@@ -3,31 +3,17 @@
         <div class="trigger" @click="popoverVisible = !popoverVisible">
         </div>
         <div class="popover" v-if="popoverVisible">
-            <div class="level1">
-                <div class="label" v-for="item1 in source" style="border: 1px solid red;" @click="level1Selected = item1">
-                    {{item1.name}}
-                </div>
-            </div>
-            <div class="level2">
-                <div class="label" v-for="item2 in level2Items" @click="level2Selected = item2">
-                    {{item2.name}}
-                </div>
-            </div>
-            <div class="level3">
-                <div class="label" v-for="item3 in level3Items">
-                    {{item3.name}}
-                </div>
-            </div>
+            <cascader-items :items="source"></cascader-items>
         </div>
     </div>
 </template>
 
 <script>
-import CascaderItem from './cascader-item'
+import CascaderItems from './cascader-items'
 export default {
     name: 'YuCascader',
     components: {
-        CascaderItem
+        CascaderItems
     },
     props: {
         source: {
