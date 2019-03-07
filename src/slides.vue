fbe686bf942b9b649bd7b1078c26f28ed7a9d2c9
yuyu <a837477816@qq.com>
2019-03-07 22:57:07 +0800

    支持上下页点击切换

--- src/slides.vue
+++ src/slides.vue
@@ -12,18 +12,28 @@
             </div>
         </div>
         <div class="yu-slides-dots">
+            <span @click="select(selectedIndex - 1)">
+                <yu-icon name="left"></yu-icon>
+            </span>
             <span 
                 v-for="n in childrenLength" 
                 :class="{active: selectedIndex === n - 1}"
                 @click="select(n-1)"
             >{{n}}</span>
+            <span @click="select(selectedIndex + 1)">
+                <yu-icon name="right"></yu-icon>
+            </span>
         </div>
     </div>
 </template>
 
 <script>
+import YuIcon from './icon'
 export default {
     name: 'YuSlides',
+    components: {
+        YuIcon
+    },
     props: {
         selected: {
             type: String
@@ -43,16 +53,19 @@ export default {
     },
     computed: {
         selectedIndex() {
-            return this.names.indexOf(this.selected) || 0
+            if (this.names.indexOf(this.selected) === -1) {
+                return 0
+            }
+            return this.names.indexOf(this.selected)
         },
         names() {
-            return this.$children.map(vm => vm.name)
+            return this.$children.filter(vm => vm.$options.name === 'YuSlidesItem').map(vm => vm.name)
         }
     },
     mounted() {
         this.updateChildren()
         this.playAutomatically()
-        this.childrenLength = this.$children.length
+        this.childrenLength = this.names.length
         this.lastSelectedIndex = this.selectedIndex
     },
     updated() {
@@ -60,16 +73,13 @@ export default {
     },
     methods: {
         onTouchStart(e) {
-            console.log('start')
             this.pause()
             if (e.touches.length > 1) return // 多指触控直接返回
             this.startTouch = e.touches[0]
         },
         onTouchMove() {
-            console.log('move')
         },
         onTouchEnd(e) {
-            console.log('end')
             let endTouch = e.changedTouches[0]
             let {clientX: x1, clientY: y1} = this.startTouch
             let {clientX: x2, clientY: y2} = endTouch
@@ -155,6 +165,7 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+        user-select: none;
         span {
             display: inline-flex;
             justify-content: center;
