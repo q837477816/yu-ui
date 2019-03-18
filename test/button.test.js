0fe4b8f6d12b538b508e81aa046ac03eddc7c8e1
yuyu <a837477816@qq.com>
2019-03-18 22:32:16 +0800

    使用vue-test-utils重写测试用例

--- test/button.test.js
+++ test/button.test.js
@@ -1,40 +1,35 @@
-const expect = chai.expect;
-import Vue from 'vue'
+import chai,{expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+chai.use(sinonChai)
+import {shallowMount, mount} from '@vue/test-utils'
 import Button from '../src/button'
+import Vue from 'vue'
 
-Vue.config.productionTip = false
-Vue.config.devtools = false
-
-describe('Button', () => {
-
+describe('Button.vue', () => {
     it('存在.', () => {
         expect(Button).to.be.exist
     })
 
     it('可以设置icon.', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        const wrapper = mount(Button, {
             propsData: {
                 icon: 'settings'
             }
-        }).$mount()
-        const useElement = vm.$el.querySelector('use')
-        expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
-        vm.$destroy()
+        })
+        const useElement = wrapper.find('use')
+        expect(useElement.attributes('href')).to.equal('#i-settings')
     })
 
     it('可以设置loading.', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        const wrapper = mount(Button, {
             propsData: {
                 icon: 'settings',
                 loading: true
             }
-        }).$mount()
-        const useElements = vm.$el.querySelectorAll('use')
-        expect(useElements.length).to.equal(1)
-        expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
-        vm.$destroy()
+        })
+        const useElement = wrapper.find('use')
+        expect(useElement.attributes('href')).to.equal('#i-loading')
     })
 
     it('icon 默认的 order 是 1', () => {
@@ -69,13 +64,12 @@ describe('Button', () => {
     })
 
     it('点击 button 触发 click 事件', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        const wrapper = mount(Button, {
             propsData: {
                 icon: 'settings',
             }
-        }).$mount()
-
+        })
+        const vm = wrapper.vm
         const callback = sinon.fake();
         vm.$on('click', callback)
         vm.$el.click()
