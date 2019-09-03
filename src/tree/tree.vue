ff169be0c9b2b8ae0349eae98bf135a44e6de22a
yuyu <a837477816@qq.com>
2019-09-03 23:33:56 +0800

    添加 tree 组件

---
+++ src/tree/tree.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="yu-tree">
+        <yu-tree-node 
+            v-for="(item, index) in cloneData" 
+            :key="index"
+            :data="item" 
+            :checkbox-visible="checkboxVisible" />
+    </div>
+</template>
+
+<script>
+import YuTreeNode from './node'
+import { deepCopy } from '../utils'
+export default {
+    name: 'YuTree',
+    components: { YuTreeNode },
+    props: {
+        data: {
+            type: Array,
+            default: () => []
+        },
+        checkboxVisible: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            cloneData: []
+        }
+    },
+    watch: {
+        data() {
+            this.rebuildData()
+        }
+    },
+    created() {
+        this.rebuildData()
+    },
+    methods: {
+        rebuildData() {
+            this.cloneData = deepCopy(this.data)
+        }
+    }
+}
+</script>
